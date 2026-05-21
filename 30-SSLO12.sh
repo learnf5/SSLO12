57344ac7a3ae9823658c478a1698f21bedc3996f
djlamey <76072289+djlamey@users.noreply.github.com>
2026-05-21 14:30:15 -0400

    Update 30-SSLO12.sh

--- 30-SSLO12.sh
+++ 30-SSLO12.sh
@@ -1,11 +1,3 @@
-
-# confirm bigip1 is active
-for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
-
-# copy archive files from GitHub to bigip1
-curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/sslo1_5_services.ucs --output /tmp/sslo1_5_services.ucs
-sudo scp /tmp/sslo1_5_services.ucs 192.168.1.31:/var/local/ucs
-
 # copy ca-cert files to internal client
 mkdir /Downloads/certs
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/certs/RootCertAndKey.pfx --output /Downloads/certs/RootCertAndKey.pfx
